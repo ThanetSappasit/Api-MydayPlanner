6d615bd2de2f0cd2bf969274646ccc3f2d6a6b37
Thanet Sappasit <thanet.sappasit@gmail.com>
2025-05-10 13:04:08 +0700

    login 0

--- controller/auth/otp.go
+++ controller/auth/otp.go
@@ -695,16 +695,12 @@ func VerifyOTP(c *gin.Context, db *gorm.DB, firestoreClient *firestore.Client) {
 		role := "user"
 		isActive := "1"
 		isVerify := "1"
-		login := 1
-		if user.Role == "admin" {
-			login = 0
-		}
 		// บันทึกหรืออัปเดตข้อมูลใน Firebase collection "usersLogin"
 		_, err = firestoreClient.Collection("usersLogin").Doc(otpRecord.Email).Set(ctx, map[string]interface{}{
 			"email":      otpRecord.Email,
 			"active":     isActive,
 			"verify":     isVerify,
-			"login":      login,
+			"login":      0,
 			"role":       role,
 			"updated_at": time.Now(),
 		}, firestore.MergeAll)
